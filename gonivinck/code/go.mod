d335c30d99208d22a2ad6303927a8d8d5f21b621
wufeng <wufeng@xiaomi.com>
2022-06-12 10:57:02 +0800

    add

--- gonivinck/code/go.mod
+++ gonivinck/code/go.mod
@@ -2,4 +2,9 @@ module code
 
 go 1.16
 
-require github.com/zeromicro/go-zero v1.3.4
+require (
+	github.com/golang/protobuf v1.5.2
+	github.com/zeromicro/go-zero v1.3.4
+	google.golang.org/grpc v1.46.2
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
+)
---
+++ gonivinck/code/service/user/api/internal/handler/registerhandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"code/service/user/api/internal/logic"
+	"code/service/user/api/internal/svc"
+	"code/service/user/api/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func RegisterHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.RegisterRequest
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+
+		l := logic.NewRegisterLogic(r.Context(), svcCtx)
+		resp, err := l.Register(req)
+		if err != nil {
+			httpx.Error(w, err)
+		} else {
+			httpx.OkJson(w, resp)
+		}
+	}
+}
